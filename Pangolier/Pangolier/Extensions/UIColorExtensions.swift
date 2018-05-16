0d063bddbf2a144e0d57db83eb4ada0e518b2030
Dmitry Baginsky <dmitri.baginski@gmail.com>
2018-05-16 23:26:20 +0300

    Added custom color

---
+++ Pangolier/Pangolier/Extensions/UIColorExtensions.swift
@@ -0,0 +1,17 @@
+//
+//  UIColorExtensions.swift
+//  Pangolier
+//
+//  Created by betraying on 5/16/18.
+//  Copyright © 2018 pangolier. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+
+  class var customRed: UIColor {
+    return #colorLiteral(red: 0.5176470588, green: 0, blue: 0.03529411765, alpha: 1)
+  }
+
+}
