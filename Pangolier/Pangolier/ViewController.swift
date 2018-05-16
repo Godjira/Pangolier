55833134007d99b800f8663b27d906db2fe5caf5
Godjira <main.godjira@gmail.com>
2018-05-16 15:52:34 +0300

    Sort v0.0.0.1

--- Pangolier/Pangolier/ViewController.swift
+++ Pangolier/Pangolier/ViewController.swift
@@ -14,35 +14,86 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 
     @IBOutlet weak var collectonView: UICollectionView!
     var heroes = [HeroModel]()
+    var groupHeroes = [[HeroModel]()]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Get instets
+        let colletctionViewLayout = self.collectonView.collectionViewLayout as! UICollectionViewFlowLayout
+        colletctionViewLayout.sectionInset = UIEdgeInsetsMake(5, 10, 5, 10)
         
         HeroManager.getHeroes { (heroesArray) in
             self.heroes = heroesArray
             self.collectonView.reloadData()
+            
+            var strHeroes = [HeroModel]()
+            var agiHeroes = [HeroModel]()
+            var intHeroes = [HeroModel]()
+            
+            for hero in self.heroes {
+                if hero.primaryAttr.elementsEqual("str"){
+                    strHeroes.append(hero)
+                } else
+                if hero.primaryAttr.elementsEqual("agi"){
+                    agiHeroes.append(hero)
+                } else {
+                    intHeroes.append(hero)
+                }
+            }
+            self.groupHeroes.append(strHeroes)
+            self.groupHeroes.append(agiHeroes)
+            self.groupHeroes.append(intHeroes)
+            
                     }
-        
     }
 
 
 
     //MARK: - UICollectionViewDataSource
     func numberOfSections(in collectionView: UICollectionView) -> Int{
-        return 3
+        return groupHeroes.count
     }
 
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return heroes.count
+        return groupHeroes[section].count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HeroCollectionViewCell", for: indexPath) as! HeroCollectionViewCell
         
-        cell.setHeroImage(hero: heroes[indexPath.row])
+        cell.setHeroImage(hero: self.groupHeroes[indexPath.section][indexPath.row])
         
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        
+        var reusableView = UICollectionReusableView()
+        var title = ""
+        var textColor = UIColor.black
+        
+        if kind == UICollectionElementKindSectionHeader {
+            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "HeaderCollectionReusableView", for: indexPath) as! HeaderCollectionReusableView
+            if indexPath.section == 1 {
+            title = "Strange"
+                textColor = UIColor.red
+            } else if indexPath.section == 2 {
+                title = "Agility"
+                textColor = UIColor.green
+            } else if indexPath.section == 3 {
+                title = "Intelect"
+                textColor = UIColor.blue
+            }
+            headerView.titleLabel.text = title
+            headerView.titleLabel.textColor = textColor
+            reusableView = headerView
+            //headerView.imageView.image = UIImage(named: )
+           
+        }
+        return reusableView
+        
+        
     }
 
 }
