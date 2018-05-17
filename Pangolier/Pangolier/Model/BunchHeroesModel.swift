6cd8b7b13a409be5cb7be6497a8fd53c85ee1806
Godjira <main.godjira@gmail.com>
2018-05-17 09:50:34 +0300

    Created struct

---
+++ Pangolier/Pangolier/Model/BunchHeroesModel.swift
@@ -0,0 +1,19 @@
+//
+//  BunchHeroesModel.swift
+//  Pangolier
+//
+//  Created by Homac on 5/17/18.
+//  Copyright © 2018 pangolier. All rights reserved.
+//
+
+import Foundation
+
+struct BunchModel {
+  
+  var id: Int
+  var name: String
+  var heroesId: [Int]
+  var description: String
+  
+  
+}
