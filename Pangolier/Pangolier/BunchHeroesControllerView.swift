4365e07dcd57ec80079df82ff208b534466309c7
Godjira <main.godjira@gmail.com>
2018-05-17 11:12:43 +0300

    [Storybord] bunch, addbunch ControllerView

---
+++ Pangolier/Pangolier/BunchHeroesControllerView.swift
@@ -0,0 +1,20 @@
+//
+//  BunchHeroesControllerView.swift
+//  Pangolier
+//
+//  Created by Homac on 5/17/18.
+//  Copyright © 2018 pangolier. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class BunchHeroesControllerView: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+  }
+  
+}
