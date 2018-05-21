37ed8700ba33fa9b77c240f278a682641cb4cb05
Dmitry Baginsky <dmitri.baginski@gmail.com>
2018-05-21 21:47:15 +0300

    Revert "Merge branch 'bunch_heroes' into development"
    This reverts commit 13ca1abdbb60ce009b739601a80a63924418e2f1, reversing
    changes made to fb6b306e3cd280fdf7feb44c9eb9d6e9cc96bf12.

--- Pangolier/Pangolier/Extensions/UIColorExtensions.swift
+++ Pangolier/Pangolier/Extensions/UIColorExtensions.swift
@@ -6,16 +6,4 @@
 //  Copyright © 2018 pangolier. All rights reserved.
 //
 
-<<<<<<< HEAD
 import Foundation
-=======
-import UIKit
-
-extension UIColor {
-
-  class var customRed: UIColor {
-    return #colorLiteral(red: 0.5176470588, green: 0, blue: 0.03529411765, alpha: 1)
-  }
-
-}
->>>>>>> 0d063bddbf2a144e0d57db83eb4ada0e518b2030
