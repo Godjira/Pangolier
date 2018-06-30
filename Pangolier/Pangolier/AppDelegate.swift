02b32d4ac55e79b8e05888ae427335803a07118d
Godjira <main.godjira@gmail.com>
2018-06-30 15:24:58 +0300

    Norm

--- Pangolier/Pangolier/AppDelegate.swift
+++ Pangolier/Pangolier/AppDelegate.swift
@@ -10,69 +10,32 @@ import UIKit
 import CoreData
 import Firebase
 import GoogleSignIn
-import FirebaseAuth
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate, GIDSignInUIDelegate {
-  
-  func sign(_ signIn: GIDSignIn!, present viewController: UIViewController!) {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
-  }
-  
   var window: UIWindow?
-  
-  func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
-    // ...
-    if let error = error {
-      // ...
-      return
-    }
-    
-    guard let authentication = user.authentication else { return }
-    let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken,
-                                                   accessToken: authentication.accessToken)
-    
-    Auth.auth().signInAndRetrieveData(with: credential) { (result, error) in
-      
-    }
-    
-    
-  }
-  
-  func sign(_ signIn: GIDSignIn!, didDisconnectWith user: GIDGoogleUser!, withError error: Error!) {
-    // Perform any operations when the user disconnects from app here.
-    // ...
-  }
-  
-  
+
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     FirebaseApp.configure()
+    Database.database().isPersistenceEnabled = true
     GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
-    GIDSignIn.sharedInstance().delegate = self
     return true
   }
-  
+
   func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey: Any] = [:]) -> Bool {
     return GIDSignIn.sharedInstance().handle(url,
                                              sourceApplication: options[.sourceApplication] as? String,
                                              annotation: [:])
-    
   }
-  
-  func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
-    return GIDSignIn.sharedInstance().handle(url,
-                                             sourceApplication: sourceApplication,
-                                             annotation: annotation)
-  }
-  
-  
+
   func applicationWillTerminate(_ application: UIApplication) {
     self.saveContext()
   }
-  
+
   // MARK: - Core Data stack
-  
+
   lazy var persistentContainer: NSPersistentContainer = {
     /*
      The persistent container for the application. This implementation
@@ -88,9 +51,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate, GIDSig
     })
     return container
   }()
-  
+
   // MARK: - Core Data Saving support
-  
+
   func saveContext () {
     let context = persistentContainer.viewContext
     if context.hasChanges {
@@ -102,6 +65,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate, GIDSig
       }
     }
   }
-  
-}
 
+}
--- Pangolier/Pangolier/Controllers/HeroesViewControllers/MainHeroesViewController.swift
+++ Pangolier/Pangolier/Controllers/HeroesViewControllers/MainHeroesViewController.swift
@@ -7,13 +7,15 @@
 //
 
 import UIKit
-import FirebaseAuth
 
 class MainHeroesViewController: BaseHeroesViewController {
-  
+
   override func viewDidLoad() {
     super.viewDidLoad()
     title = "Choose hero"
+
+    let item = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(MainHeroesViewController.addButtonAction))
+    navigationItem.rightBarButtonItem = item
   }
 
   func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
@@ -23,6 +25,12 @@ class MainHeroesViewController: BaseHeroesViewController {
     heroVC.hero = selectedHero
     heroVC.allHeroes = self.heroes
     navigationController?.pushViewController(heroVC, animated: true)
+  }
+
+  @objc func addButtonAction() {
+    guard let addBunchVC = storyboard?
+      .instantiateViewController(withIdentifier: "AddBunchHeroesViewController") as? AddBunchHeroesViewController else { return }
+    navigationController?.pushViewController(addBunchVC, animated: true)
   }
 
 }
